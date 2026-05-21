3571e110c9583351739ad267ce1750291cf20908
JuraMit <Uramirukov200@gmail.com>
2026-05-21 13:33:23 +0800

    Update l1.pl

--- Л1/l1.pl
+++ Л1/l1.pl
@@ -1,26 +1,27 @@
-% Факты
+% ===== ФАКТЫ =====
 father(john, jim).
 father(john, ann).
+
 father(bob, mike).
 father(bob, lisa).
+
 father(mike, tom).
 father(mike, kate).
+
 father(jim, peter).
+
 father(david, emma).
 
-brother_fact(jim, ann).
-brother_fact(mike, lisa).
-brother_fact(tom, kate).
-brother_fact(peter, jim).
 
-% Правила
+% ===== ПРАВИЛА =====
 brother(X, Y) :-
-    brother_fact(X, Y);
-    brother_fact(Y, X).
+    father(F, X),
+    father(F, Y),
+    X \= Y.
 
-is_father(X, Y) :- father(X, Y).
+is_father(X, Y) :-
+    father(X, Y).
 
 uncle(X, Y) :-
     brother(X, Parent),
-    is_father(Parent, Y),
-    X \= Y.
+    father(Parent, Y).
